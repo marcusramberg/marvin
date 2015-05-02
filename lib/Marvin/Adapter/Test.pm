9d66472a5d76b0a3a7eef43f912fe16f850d4976
Marcus Ramberg <marcus.ramberg@usit.uio.no>
2015-05-02 16:14:24 +0200

    Stubbed test adapter. Removed uio config from sample

---
+++ lib/Marvin/Adapter/Test.pm
@@ -0,0 +1,16 @@
+package Marvin::Adapter::Test;
+
+use Mojo::Base 'Marvin::Adapter';
+use experimental 'signatures';
+
+sub register($self, $app) {
+
+}
+
+=head1 NAME
+
+Marvin::Adapter::Test- Test Adapter for Marvin.
+
+=head1 SYNOPSIS
+
+1;
